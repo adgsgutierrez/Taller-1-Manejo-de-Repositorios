5ea6c36e57b90fa995e98ae51f6ab9553ae8752e
PORTATIL-ADGS\ADGS <adgsgutierrez@gmail.com>
2017-03-02 07:25:48 -0500

    Se estructura el proyecto

--- Taller1ManejodeRepositorios/src/taller1manejoderepositorios/taller1manejoderepositorios/Taller1ManejodeRepositorios.java
+++ Taller1ManejodeRepositorios/src/taller1manejoderepositorios/taller1manejoderepositorios/Taller1ManejodeRepositorios.java
@@ -14,8 +14,20 @@ public class Taller1ManejodeRepositorios {
     /**
      * @param args the command line arguments
      */
+    public static Recipes recepies = new Recipes();
+    
+
     public static void main(String[] args) {
         // TODO code application logic here
+        searchByName window = new searchByName();
+        window.setVisible(true);
+        window.setTitle("Formulario de Busqueda");
+        
+        busquedaPorIngrediente ventana=new busquedaPorIngrediente();
+        ventana.setVisible(true);
+        ventana.setTitle("Busqueda de receta por ingrediente");
+        Recipes r=new Recipes();
+        System.out.println(r.recipes);   
     }
     
 }
